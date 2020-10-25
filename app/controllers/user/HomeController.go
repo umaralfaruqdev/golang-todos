8276f34cda60eca0217c665da58fef2cfc74004b
umaralfaruqdev <ualfaruq59@gmail.com>
2020-10-25 10:27:18 +0700

    parsing template

--- app/controllers/user/HomeController.go
+++ app/controllers/user/HomeController.go
@@ -2,8 +2,24 @@ package user
 
 import (
     "net/http"
+    "os"
+    "html/template"
+    . "gotodo/system/error"
 )
 
 func UserIndex(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("hello world"))
+    var data = struct {
+        Name string
+    } {
+        Name: "Umar",
+    }
+
+    var dir, _ = os.Getwd()
+
+    var tmpl, err = template.ParseFiles(""+dir+"/app/views/user/index.html")
+    if !Msg(err) { return }
+
+    err = tmpl.Execute(w, data)
+    if !Msg(err) { return }
+
 }  
