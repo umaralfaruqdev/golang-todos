2877c644b390af70719816c0defad63650a96549
umaralfaruqdev <ualfaruq59@gmail.com>
2020-10-29 10:07:40 +0700

    change type of isMarried, create resFunc, send status code

--- app/controllers/user/CrudController.go
+++ app/controllers/user/CrudController.go
@@ -6,7 +6,6 @@ import (
 	userModel "gotodo/app/models/user"
 	. "gotodo/system/error"
 	"encoding/json"
-	"reflect"
 )
 
 func init() {
@@ -17,21 +16,33 @@ type UserStuct struct {
 	Fname 	  string `json:"fname"`
 	Lname 	  string `json:"lname"`
 	Born 	  string `json:"born"`
-	IsMarried bool   `json:"is_married"`
+	IsMarried string   `json:"is_married"`
 }
 
 func UserStore(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
+
+	// initial varaiables
+	var errConv string
+
+	// the function for write response
+	// @parameter status_code, message
+	var resFunc = func(str ...string) (resByte []byte) {
+		var resStr string = `{"status": `+str[0]+`, "message": "`+str[1]+`"}`
+		resByte = []byte(resStr)
+		
+		return
+	}
 	
 	var data UserStuct // use struct
-	var jsonString string = `{"status": 200, "message": "your data successfuly inserted"}`
-	var jsonByte []byte = []byte(jsonString)
 
 	var decoder *json.Decoder = json.NewDecoder(r.Body)
 	var err error = decoder.Decode(&data)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.WriteHeader(500)
+		errConv = fmt.Sprintf("%s", err)
+		w.Write(resFunc("500", errConv))
 		return
 	}
 
@@ -50,18 +61,22 @@ func UserStore(w http.ResponseWriter, r *http.Request) {
 
 	if isMaried == "" {
 		isMaried = nil
+	} else if isMaried == "false" {
+		isMaried = false
+	} else if isMaried == "true" {
+		isMaried = true
 	}
 
 	// insert to database
 	err = userModel.UserStore(fname, lname, born, isMaried)
-	var jsonStringErr = fmt.Sprintf(`{"status": %d, "message": "%v"}`, 403, err)
-	var jsonStringErrByte = []byte(jsonStringErr)
 
 	if !Msg(err) {
-		w.Write(jsonStringErrByte)
-		// http.Error(w, err.Error(), 403)
+		w.WriteHeader(http.StatusForbidden)
+		errConv = fmt.Sprintf("%s", err)
+		w.Write(resFunc("403", errConv))
 		return
 	}
 
-	w.Write(jsonByte)
+	w.WriteHeader(http.StatusOK)
+	w.Write(resFunc("200", "Success"))
 }
